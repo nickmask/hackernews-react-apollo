1ab418f2e424314fda217bac94a59768f5aed9b5
Nick Maskell <nick.maskell@xero.com>
2017-07-18 17:16:08 +1200

    Add progress, define is not defined issue is happening

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React, { Component } from 'react'
+import LinkList from './LinkList'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <LinkList />
+    )
   }
 }
 
-export default App;
+export default App
