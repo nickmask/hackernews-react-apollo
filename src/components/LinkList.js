58dc846d4d1c677c2cdbe43ff1b08768ada9186c
Nick Maskell <nick.maskell@xero.com>
2017-08-09 11:10:32 +1200

    Add subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,63 @@ import Link from './Link'
 
 class LinkList extends Component {
 
+  componentDidMount() {
+    this._subscribeToNewLinks()
+  }
+  
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // 1
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+    
+    // 2
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+    
+    // 3
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+
+  _subscribeToNewLinks = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Link(filter: {
+            mutation_in: [CREATED]
+          }) {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [
+          subscriptionData.data.Link.node,
+          ...previous.allLinks
+        ]
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        }
+        return result
+      }
+    })
+  }
+
+
   render () {
     console.log(this.props.allLinksQuery)
     if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
@@ -18,22 +75,31 @@ class LinkList extends Component {
 
     return (
       <div>
-        {linksToRender.map(link => (
-          <Link key={link.id} link={link} />
+        {linksToRender.map((link, index) => (
+          <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote}  index={index} link={link} />
         ))}
       </div>
     )
   }
 }
 
-const ALL_LINKS_QUERY = gql`
-  # 2
+export const ALL_LINKS_QUERY = gql`
   query AllLinksQuery {
     allLinks {
       id
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
 `
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import App from './components/App'
 import { GC_AUTH_TOKEN } from './constants'
 
@@ -12,8 +13,20 @@ const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj5928cqqhndf01051huyv3k4'
 })
 
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj5928cqqhndf01051huyv3k4', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
+
 networkInterface.use([{
-  applyMiddleware (req, next) {
+  applyMiddleware(req, next) {
     if (!req.options.headers) {
       req.options.headers = {}
     }
@@ -24,7 +37,7 @@ networkInterface.use([{
 }])
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 ReactDOM.render(
